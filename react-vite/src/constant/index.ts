d7ac17c17b355a928423dad1a8879dd492ad23fa
Ogheneochuko <7204868+amochuko@users.noreply.github.com>
2024-07-25 16:12:10 +0100

    fix: refactored for new api

--- react-vite/src/constant/index.ts
+++ react-vite/src/constant/index.ts
@@ -1,4 +1,3 @@
 const API_URL = 'http://localhost:1633';
-const API_DEBUG_URL = 'http://127.0.0.1:1635';
 
-export { API_DEBUG_URL, API_URL };
+export { API_URL };
--- react-vite/src/utils/bee-node.ts
+++ react-vite/src/utils/bee-node.ts
@@ -1,7 +1,6 @@
-import { Bee, BeeDebug } from "@ethersphere/bee-js";
-import { API_DEBUG_URL, API_URL } from "../constant";
+import { Bee } from "@ethersphere/bee-js";
+import { API_URL } from "../constant";
 
 const bee = new Bee(API_URL);
-const beeDebug = new BeeDebug(API_DEBUG_URL);
 
-export { bee, beeDebug };
+export { bee };
