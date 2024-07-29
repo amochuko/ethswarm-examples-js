9e4deab8ae220b167b8ee0f82077bbc1c01a7768
Ogheneochuko <7204868+amochuko@users.noreply.github.com>
2024-07-29 18:20:33 +0100

    fix: updated tags

--- react-vite/src/components/Header.tsx
+++ react-vite/src/components/Header.tsx
@@ -1,9 +1,11 @@
+import GetBalance from "./GetBalance";
 import { NodeStatus } from "./NodeStatus";
 
 function Header() {
   return (
-    <header>
+    <header className="flex flex-row justify-around my-8 p-8">
       <NodeStatus />
+      <GetBalance />
     </header>
   );
 }
--- react-vite/src/components/NodeStatus.tsx
+++ react-vite/src/components/NodeStatus.tsx
@@ -3,16 +3,14 @@ import { useNodeHealth } from "../hooks/useNodeHealth";
 export function NodeStatus() {
   const { nodeHealth } = useNodeHealth();
 
-
   return (
-    <section className="connection-status">
+    <div className="connection-status">
       <p>
-        Node status:{" "}
         <span
           className={nodeHealth?.status == "ok" ? "online" : "offline"}
         ></span>
         {nodeHealth?.status == "ok" ? "Connected" : "Disconnected"}
       </p>
-    </section>
+    </div>
   );
 }
