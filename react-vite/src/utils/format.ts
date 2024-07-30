fd2abfc13441d3075cd879469d0ed4a167bdbd18
Ogheneochuko <7204868+amochuko@users.noreply.github.com>
2024-07-30 19:11:35 +0100

    fix: updated clipboard function

--- react-vite/src/utils/format.ts
+++ react-vite/src/utils/format.ts
@@ -1,9 +1,41 @@
-export const trimText = (txt: string) => {
-  return txt.length > 20 ? txt.slice(0, 22) + "..." : txt;
-};
-export const copyText = async () => {
-  if (navigator.clipboard) {
-    await navigator.clipboard.readText();
-    console.log("copied text");
-  }
+export const format = {
+  parsePlur: (n: number) => {
+    return n * 10 ** 16;
+  },
+  parsexBZZ: (n: number) => {
+    return n * 1e18;
+  },
+  formatPlur: (n: number) => {
+    return n / 10 ** 16;
+  },
+  copyText: async (txt: string) => {
+    return new Promise((res, rej) => {
+      if (navigator.clipboard) {
+        navigator.clipboard
+          .writeText(txt)
+          .then(() => res(true))
+          .catch((err) => rej(err));
+        console.log("copied text");
+      } else {
+        // fallback
+        const txtarea = document.createElement("textarea");
+        txtarea.value = txt;
+        document.body.appendChild(txtarea);
+        txtarea.select();
+
+        try {
+          document.execCommand("copy");
+          res(true);
+        } catch (err) {
+          document.removeChild(txtarea);
+          rej(err);
+        }
+      }
+    });
+  },
+  trimText: (txt: string) => {
+    return txt.length > 16
+      ? txt.slice(0, 10) + "..." + txt.substring(txt.length - 10)
+      : txt;
+  },
 };
