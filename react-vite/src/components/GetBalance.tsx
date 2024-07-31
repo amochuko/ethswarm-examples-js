01de0c00dbe2fbefcd74259eb85c2fd4b21f8f28
Ogheneochuko <7204868+amochuko@users.noreply.github.com>
2024-07-31 13:28:06 +0100

    fix: updated with node status

--- react-vite/src/components/GetBalance.tsx
+++ react-vite/src/components/GetBalance.tsx
@@ -1,6 +1,11 @@
+import { Health } from "@ethersphere/bee-js";
 import { useWallet } from "../hooks/useWallet";
+import HandleCopy from "./HandleCopy";
 
-export default function GetBalance() {
+type NodeStatusProps = {
+  nodeHealth: Health | null | undefined;
+};
+export default function GetBalance(props: NodeStatusProps) {
   const { balance, error, isLoading } = useWallet();
 
   const bzzBalance = () => Number(balance?.bzzBalance) / 10 ** 16;
@@ -8,21 +13,30 @@ export default function GetBalance() {
     Number(balance?.nativeTokenBalance) / 10 ** 18;
 
   return (
-    <div>
-      <p>{isLoading && "Loading balance..."}</p>
-      <p>{error && "Error loading balance..."}</p>
-      {!isLoading && !error && (
-        <div className="flex flex-col space-y-1">
-          <span>{bzzBalance().toString().substring(0, 6)} xBzz</span>
-          <span>{nativeTokenBalance().toString().substring(0, 6)} xDai</span>
-          <div className="tooltip">
-            {balance?.walletAddress.substring(0, 6)}...
-            {balance?.walletAddress.substring(36)}
-            <span className="tooltip-text">{balance?.walletAddress}</span>
-          </div>
-        </div>
+    <div className="">
+      {props.nodeHealth?.status === "ok" && (
+        <>
+          <p>{isLoading && "Loading balance..."}</p>
+          <p>{error && "Error loading balance..."}</p>
+
+          {!isLoading && !error && (
+            <div className="flex flex-col space-y-1">
+              <span>{bzzBalance().toString().substring(0, 6)} xBzz</span>
+              <span>
+                {nativeTokenBalance().toString().substring(0, 6)} xDai
+              </span>
+              <div className="tooltip">
+                {balance?.walletAddress.substring(0, 6)}...
+                {balance?.walletAddress.substring(36)}
+                <span className="tooltip inline-block">
+                  <HandleCopy txt={String(balance?.walletAddress)} />
+                  <span className="tooltip-text">{balance?.walletAddress}</span>
+                </span>
+              </div>
+            </div>
+          )}
+        </>
       )}
     </div>
   );
 }
-
