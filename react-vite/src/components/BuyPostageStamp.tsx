f9e4a150b48cf09376bb9e69d5fac159f068a4c2
Ogheneochuko <7204868+amochuko@users.noreply.github.com>
2024-07-31 13:31:27 +0100

    fix: update min depth value

--- react-vite/src/components/BuyPostageStamp.tsx
+++ react-vite/src/components/BuyPostageStamp.tsx
@@ -1,11 +1,13 @@
 import { useEffect, useState } from "react";
 import { usePostageBatch } from "../hooks/usePostageBatch";
+import { format } from "../utils/format";
 
 type State = {
   amount: number | string;
   depth: number | string;
   label?: string;
   mutable: boolean;
+  [index: string]: any;
 };
 
 export const BuyPostageStamp = () => {
@@ -14,7 +16,7 @@ export const BuyPostageStamp = () => {
   const [buyBtn, setBuyBtn] = useState(false);
   const [values, setValues] = useState<State>({
     amount: "",
-    depth: "",
+    depth: minimumBatchDepthValue,
     label: "",
     mutable: false,
   });
@@ -39,24 +41,77 @@ export const BuyPostageStamp = () => {
     const name = target.name;
     const value = target.value;
 
-     setValues((preState) => ({ ...preState, [name]: value }));
+    console.log(name, value);
+    setValues((preState) => ({ ...preState, [name]: value }));
   };
 
   const handleBuyPostage = async () => {
-    values.amount = Number(values.amount) * 1e18;
+    if (+values.depth < 17) {
+      alert("Minimal depth is 17");
+    }
+    if (values.amount == "" && values.label == "" && values.depth == "") {
+      console.log("noting... ");
+      return;
+    } else {
+      values.amount = format.parsexBZZ(Number(values.amount));
+      console.log("values... ", values);
 
-    const { amount, depth, mutable, label } = values;
+      // const { amount, depth, mutable, label } = values;
+      // await buyPostageBatch({
+      //   amount,
+      //   depth: Number(depth),
+      //   options: {
+      //     label,
+      //     immutableFlag: Boolean(mutable),
+      //   },
+      // });
 
-    await buyPostageBatch({
-      amount,
-      depth: Number(depth),
-      options: {
-        label,
-        immutableFlag: Boolean(mutable),
-      },
-    });
+      setValues({ amount: "", depth: 17, label: "", mutable: false });
+    }
+  };
 
-    setValues({ amount: "", depth: "", label: "", mutable: false });
+  const handleCostEstimate = () => {
+    // PostageBatchBucket = 2 ** bucketDepth
+
+    // batchDepth :~ how much data can be stored (stamped) by a batch =  2 ** batchDepth
+    // ex: if batchDepth = 24, then 2 ** 24 of chunks can be stored.
+    // theoretical maxBatchVolume = (2 ** batchDept) * 4kb (one chunk)
+
+    // bucketDepth :~ how many chunks are allowed in each bucket = 2 ** (batchDepth - bucketDepth)
+    // ex: with batchDepth = 24, and bucketDepth = 16
+    // 2 ** (24 - 16) = 2 ** 8 = 256 chucks/bucket
+
+    const minimumBatchDepthValue = 21;
+    const dataOrChunkSize = 2 ** minimumBatchDepthValue; // can be stamped
+
+    // batch Amount (batchCost) : amount of xBZZ in PLUR (1 * 10**16) per chunk in the batch
+    // total batchAmount paid for a batch = (2 ** batchDepth) * amount (xBZZ in PLUR)
+    // example:
+    // batchDepth = 24, amount = 1000000000 PLUR
+    // total batchAmount = (2 ** 24) * 1000000000 PLUR = 16777216000000000 PLUR ~ 1.6777216 xBZZ
+
+    // Estimating `amount` needed for desired TTL
+    // (stampPrice / gnosisBlocktimeInSeconds) * storageTimeInSeconds (duration)
+    // ex: stampPrice = 24000 PLUR, blocktime = 5, duration = 12 days (1 day = 24 hr * 60 min * 60 secs)
+    // ttlAmountNeeded for 4 days = (24000 / 5) * (5 * 24 * 60 * 60) = 4976640000 PLUR
+
+    const secsPerDay = 24 * 60 * 60;
+    const ttlInDays = 12;
+    const storageTimeInSeconds = ttlInDays * secsPerDay;
+    const gnosisBlocktimeInSeconds = 5;
+    const postpageStampPrice = 24000;
+
+    const ttlAmountNeeded =
+      (postpageStampPrice / gnosisBlocktimeInSeconds) * storageTimeInSeconds;
+
+    // console.log("ttlAmountNeeded: ", format.parsePlur(ttlAmountNeeded));
+
+    const batchDepth = 24;
+    const totalBatchAmount = ttlAmountNeeded * 2 ** batchDepth;
+    const pricePerGBPerMonth = 0.00005435817984;
+    const price = pricePerGBPerMonth * 2 ** batchDepth;
+
+    // console.log("totalBatchAmount: ", format.formatPlur(16777216000000000));
   };
 
   return (
@@ -76,9 +131,9 @@ export const BuyPostageStamp = () => {
         <>
           <div className="flex flex-col justify-between sm:flex-row ">
             <div className="flex-col space-y-2 mb-4 text-stone-500 font-medium sm:min-w-[45%]">
-              <label htmlFor="label">Batch </label>
+              <label htmlFor="label">Batch Label </label>
               <input
-                className="p-4  border-[1px] w-full"
+                className="p-4 border-[1px] w-full"
                 type="text"
                 value={values["label"]}
                 id="label"
@@ -95,11 +150,11 @@ export const BuyPostageStamp = () => {
                 className="p-4 border-[1px] w-full"
                 onChange={handleOnchange}
               >
-                <option defaultValue={" Select size in GB"} disabled>
+                <option value={" Select size in GB"} style={{ color: "red" }}>
                   Select size in GB
                 </option>
                 {Array.from([1, 2, 3, 4, 5, 10, 12, 15]).map((e, i) => (
-                  <option key={i} value={e * 2} selected={i === 0}>
+                  <option key={i} value={e * 2}>
                     {e * 2} GB
                   </option>
                 ))}
@@ -108,19 +163,20 @@ export const BuyPostageStamp = () => {
           </div>
           <div className="flex flex-col justify-between sm:flex-row">
             <div className="flex-col space-y-2 mb-4 text-stone-500 font-medium sm:min-w-[45%]">
-              <label htmlFor="depth">Duration</label>
+              <label htmlFor="depth">Depth</label>
               <input
-                className="p-4  border-[1px] w-full"
+                className="p-4 border-[1px] w-full"
                 type="number"
                 value={values["depth"]}
                 id="depth"
                 name="depth"
                 placeholder="Number of days"
+                min={minimumBatchDepthValue}
                 onChange={handleOnchange}
               />
             </div>
             <div className="flex-col space-y-2 text-stone-500 font-medium sm:min-w-[45%]">
-              <label htmlFor="mutable">Mutable</label>
+              <label htmlFor="mutable">Is Mutable</label>
               <select
                 name="mutable"
                 id="mutable"
@@ -130,13 +186,13 @@ export const BuyPostageStamp = () => {
                 <option
                   disabled
                   className="text-slate-600 font-semibold"
-                  defaultValue={"Select option"}
+                  value={"Select option"}
                 >
                   Select option
                 </option>
                 {Array.from(["false", "true"]).map((e, i) => (
-                  <option value={e} key={i} selected={i === 0}>
-                    {e}
+                  <option defaultValue={e} key={i} selected={i === 0}>
+                    {e.toUpperCase()}
                   </option>
                 ))}
               </select>
@@ -157,14 +213,6 @@ export const BuyPostageStamp = () => {
             >
               {isBuyPostageBatch ? "Process..." : "Buy"}
             </button>
-            {/* <button
-              className={`text-xl border-2 border-solid bg-gray-600 text-white w-32 p-4`}
-              type="submit"
-              onClick={handleBuyPostage}
-            >
-              {"Buy Me"}
-            </button> */}
-
             <button
               className={`text-xl border-2 border-solid bg-gray-600 text-white w-32 p-4 hover:bg-yellow-500 hover:cursor-pointer hover:text-black`}
               type="submit"
