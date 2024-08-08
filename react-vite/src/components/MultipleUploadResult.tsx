404629cbdf55af58cf9889ca9112ba4d4b5ca45b
Ogheneochuko <7204868+amochuko@users.noreply.github.com>
2024-08-08 04:47:59 +0100

    fix: update link to a manifest hashes

--- react-vite/src/components/MultipleUploadResult.tsx
+++ react-vite/src/components/MultipleUploadResult.tsx
@@ -10,26 +10,32 @@ type MultipleUploadResultProps = {
 export default function MultipleUploadResult(props: MultipleUploadResultProps) {
   const { getHashes } = useManifest();
   const [refHashes, setRefHashes] = useState<Record<string, string>>({});
+  const [isProcessing, setIsProcessing] = useState(false);
 
   useEffect(() => {
     getHashes(props.uploadResultWithCid.reference)
       .then((hash) => {
+        setIsProcessing(true);
         setRefHashes(hash);
       })
-      .catch((err) => console.error(err));
-
+      .catch((err) => console.error(err))
+      .finally(() => {
+        setIsProcessing(false);
+      });
   }, [props.uploadResultWithCid]);
 
   const getHashDetails = () => {
     return Object.keys(refHashes).map((key, i) => (
       <li key={i + key} className="text-gray-900 truncate hover:text-clip">
-        <span className="font-semibold"> {i+1}. {key}: </span>{" "}
+        <span className="font-semibold">
+          {i + 1}. {key}:{" "}
+        </span>{" "}
         <a
-          href={`${props.link}/${refHashes[key]}`}
+          href={`${props.link}/${props.uploadResultWithCid.reference}/${key}`}
           target="_blank"
           referrerPolicy="same-origin"
         >
-          {`${props.link}/${refHashes[key]}`}
+          {`${props.link}/${props.uploadResultWithCid.reference}/${key}`}
         </a>
       </li>
     ));
@@ -37,10 +43,16 @@ export default function MultipleUploadResult(props: MultipleUploadResultProps) {
 
   return (
     <div className="flex flex-col space-y-2 bg-slate-200 p-8">
-      <h2 className="text-2xl mb-6">Preview file</h2>
-      <ul className="space-y-4">
-        {getHashDetails()}
-      </ul>
+      {!isProcessing ? (
+        <>
+          <h2 className="text-2xl mb-6">Preview file</h2>
+          <ul className="space-y-4">{getHashDetails()}</ul>
+        </>
+      ) : (
+        <p className="text-center font-medium text-gray-900 py-8">
+          Processing manifest...
+        </p>
+      )}
     </div>
   );
 }
