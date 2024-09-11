457848d120f46da92df599d0c1eb72b9dd6b91fe
Ogheneochuko <7204868+amochuko@users.noreply.github.com>
2024-09-11 01:09:19 +0100

    update: add BeeContext provider

--- react-webpack/src/App.tsx
+++ react-webpack/src/App.tsx
@@ -1,7 +1,97 @@
-import React from "react";
-import Home from "./components/Home";
+import { Bee } from "@ethersphere/bee-js";
+import React, { useEffect, useState } from "react";
 import "./App.css";
+import Home from "./components/Home";
+import { BeeContext } from "./context/beeContext";
+import utils from "./utils";
 
 export default function App() {
-  return <Home />;
+  const [bee, setBee] = useState<Bee>();
+  const [cnxError, setCnxError] = useState("");
+  const [nodeUrl, setNodeUrl] = useState("");
+  const [savingUrl, setSavingUrl] = useState(false);
+  const [alive, setAlive] = useState(false);
+
+  useEffect(() => {
+    const url = utils.getBeeNodeUrl();
+    if (url) {
+      setBee(new Bee(url));
+    }
+  }, [nodeUrl]);
+
+  useEffect(() => {
+    bee
+      ?.checkConnection()
+      .then(() => {
+        setAlive(true);
+      })
+      .catch((err) => {
+        setCnxError(err);
+      });
+  }, [bee, alive]);
+
+  const handleSaveToLocalhost = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    e.preventDefault();
+
+    setSavingUrl(true);
+    utils.setBeeNodeUrl(nodeUrl);
+
+    setSavingUrl(false);
+    setNodeUrl("");
+  };
+
+  return (
+    <>
+      {bee ? (
+        <BeeContext.Provider value={bee}>
+          <Home />
+        </BeeContext.Provider>
+      ) : (
+        <div className="app-container">
+          <div className="app-container-chaid">
+            <h1>Set up URL to Bee Node</h1>
+            <p>
+              The URL is used to connect to an active Bee Node and would be
+              saved to your browser local storage.
+            </p>
+            <form>
+              <input
+                type="text"
+                id="beeNodeUrl"
+                placeholder="Enter Bee Node URL"
+                onChange={(e) => setNodeUrl(e.target.value)}
+              />
+              <button
+                style={{
+                  width: "100%",
+                  fontSize: 20,
+                  cursor: `${nodeUrl === "" ? "not-allowed" : "pointer"}`,
+                }}
+                disabled={nodeUrl === ""}
+                onClick={handleSaveToLocalhost}
+              >
+                {savingUrl ? "Saving..." : "Save"}
+              </button>
+            </form>
+
+            {cnxError && (
+              <p
+                style={{
+                  color: "tomato",
+                  fontWeight: 600,
+                  backgroundColor: "rgba(255,0,0,0.08)",
+                  padding: "8px",
+                }}
+              >
+                {String(cnxError).split(":")[1]}: There is no live Bee node on
+                the given URL!
+              </p>
+            )}
+          </div>
+        </div>
+      )}
+    </>
+  );
 }
