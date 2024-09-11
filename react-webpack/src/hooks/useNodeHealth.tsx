8de7f8814eddb71353f7d528f998bd4dffa40d51
Ogheneochuko <7204868+amochuko@users.noreply.github.com>
2024-09-11 01:26:53 +0100

    fix: update with useContext

--- react-webpack/src/hooks/useNodeHealth.tsx
+++ react-webpack/src/hooks/useNodeHealth.tsx
@@ -1,13 +1,13 @@
 import { Health } from "@ethersphere/bee-js";
-import { useEffect, useState } from "react";
-import utils from "../utils";
+import { useContext, useEffect, useState } from "react";
+import { BeeContext } from "../context/beeContext";
 
 export const useNodeHealth = () => {
   const [nodeHealth, setNodeHealth] = useState<Health | null>();
   const [isLoading, setIsLoading] = useState(false);
   const [healthError, setHealthError] = useState("");
 
-  const bee = utils.getBee();
+  const bee = useContext(BeeContext);
 
   useEffect(() => {
     handleIsNodeOnline();
@@ -16,13 +16,10 @@ export const useNodeHealth = () => {
   const handleIsNodeOnline = async () => {
     try {
       setIsLoading(true);
-      const health = await bee.getHealth();
-
+      const health = await bee!.getHealth();      
       setNodeHealth(health);
-    } catch (err:any) {
-      console.error("erro101:", err.statusText);
+    } catch (err: any) {
       setHealthError(err.statusText);
-
     } finally {
       setIsLoading(false);
     }
